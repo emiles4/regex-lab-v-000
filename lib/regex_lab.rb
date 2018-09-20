d6da6ed568a3f20059863d657ab27e729c66a230
Emily Miles <emiles4@gmail.com>
2018-09-20 05:16:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,15 +3,15 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan (/un\w+ing/i)
+  text.scan(/un\w+ing/i)
 end
 
 def words_five_letters_long(text)
-  text.scan (/\b[a-z]{5}\b/i)
+  text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match (/^[A-Z].+\.$/) ? true : false
+  text.match(/^[A-Z].+\.$/) ? true : false
 end
 
 def valid_phone_number?(phone)
